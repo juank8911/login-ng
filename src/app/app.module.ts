e3211adf6bbd01a62fd598637512fd295e965914
juank8911 <juancarlos_891111@hotmail.com>
2022-03-16 01:08:39 -0500

    Agregando RouterLink y navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
@@ -12,7 +15,9 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'login-ng-88909',
+    appId: '1:956533493470:web:8662dd0fe169427f85fc4f',
+    storageBucket: 'login-ng-88909.appspot.com',
+    apiKey: 'AIzaSyC18QUOxSHg_1jQQJenrzgqmr1qtMnMp9A',
+    authDomain: 'login-ng-88909.firebaseapp.com',
+    messagingSenderId: '956533493470',
+  },
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyC18QUOxSHg_1jQQJenrzgqmr1qtMnMp9A",
